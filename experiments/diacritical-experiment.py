bb48ec623fbfeffd93bba9db1fc0cb2944b448fb
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2021-11-12 02:51:46 +0000

    Add toxic experiment

--- experiments/diacritical-experiment.py
+++ experiments/diacritical-experiment.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 import pickle
+import pandas as pd
 from abc import ABC
 from typing import Callable, List, Dict, Tuple
 from textdistance import levenshtein
-from torch import cuda
+from torch import cuda, device as torchdevice
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from datasets import load_dataset
 from PIL import Image, ImageDraw, ImageFont
-from time import process_time, sleep
+from time import process_time
 from argparse import ArgumentParser
 from scipy.optimize import differential_evolution
 from os.path import exists
 from tqdm.auto import tqdm
+from toxic.core.model import ModelWrapper
+from logging import getLogger, WARNING
 
 # Diacritical marks supported by Arial Unicode MS
 # Full range is 768 - 879
@@ -30,13 +33,29 @@ def load_font(font: str, font_size: int) -> None:
     d.text((20, 5), text, font=font, fill=0)
     return img
 
-def load_glue_data(start_index: int, end_index: int,):
+def load_glue_data(start_index: int, end_index: int):
   return load_dataset('glue', 'cola', split='validation') \
     .filter(lambda x: x['label'] == 1) \
     .remove_columns(['label']) \
     .select(range(start_index, end_index))
 
-def serialize(adv_example: str, adv_example_ocr: str, input: str, input_ocr: str, adv_generation_time: int, budget: int, maxiter: int, popsize: int):
+def load_toxic_data(start_index: int, end_index: int):
+    # Load toxic comments data set
+    getLogger('numexpr.utils').setLevel(WARNING)
+    comments = pd.read_csv('toxicity_annotated_comments.tsv', sep = '\t', index_col = 0)
+    annotations = pd.read_csv('toxicity_annotations.tsv',  sep = '\t')
+    # labels a comment as toxic if the majority of annoatators did so
+    labels = annotations.groupby('rev_id')['toxicity'].mean() > 0.5
+    # join labels and comments
+    comments['toxicity'] = labels
+    # remove newline and tab tokens
+    comments['comment'] = comments['comment'].apply(lambda x: x.replace("NEWLINE_TOKEN", " "))
+    comments['comment'] = comments['comment'].apply(lambda x: x.replace("TAB_TOKEN", " "))
+    test_comments = comments.query("split=='test'").query("toxicity==True")
+    examples = test_comments.reset_index().to_dict('records')
+    return examples[start_index:end_index]
+
+def serialize_trocr(adv_example: str, adv_example_ocr: str, input: str, input_ocr: str, adv_generation_time: int, budget: int, maxiter: int, popsize: int, **kwargs):
   return  {
     'adv_example': adv_example,
     'adv_example_ocr': adv_example_ocr,
@@ -48,6 +67,15 @@ def serialize(adv_example: str, adv_example_ocr: str, input: str, input_ocr: str
     'budget': budget,
     'maxiter': maxiter,
     'popsize': popsize
+  }
+
+def serialize_toxic(adv_label_toxic: bool, gold_label_toxic: bool, adv_logit_toxic: float, **kwargs):
+  base = serialize_trocr(**kwargs)
+  return {
+    'adv_label_toxic': adv_label_toxic,
+    'gold_label_toxic': gold_label_toxic,
+    'adv_logit_toxic': adv_logit_toxic,
+    **base
   }
 
 
@@ -101,7 +129,7 @@ class OcrObjective(ABC):
       input_ocr = self.ocr(self.input)
       adv_example_ocr = input_ocr
       generation_time = 0
-    return serialize(adv_example, adv_example_ocr, self.input, input_ocr, generation_time, self.budget, maxiter, popsize)
+    return serialize_trocr(adv_example, adv_example_ocr, self.input, input_ocr, generation_time, self.budget, maxiter, popsize)
 
 
 class TrOcrObjective(OcrObjective):
@@ -119,20 +147,33 @@ class TrOcrObjective(OcrObjective):
     del pixel_values # remove from GPU
     return self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
+class ToxicOcrObjective(TrOcrObjective):
 
-def trocr_experiment(start_index: int, end_index: int, min_budget: int, max_budget: int, pkl_file: str, maxiter: int, popsize: int, overwrite: bool, cpu: bool, **kwargs):
-  # Load resources
-  label = "trocr-diacriticals"
-  dataset = load_glue_data(start_index, end_index)
-  print(f"Performing experiments against the TrOCR model for {len(dataset)} examples, from index {start_index} to {end_index}, with budgets {min_budget} to {max_budget}.")
-  # The "printed" models output all uppercase, so we use the "handwritten" models
-  # which appear to perform well on computer-generated text as well
-  device = 'cuda:0' if not cpu and cuda.is_available() else 'cpu'
-  processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
-  model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten').to(device)
-  print(f"TrOCR configured to use device {device}.")
-  budgets = range(min_budget, max_budget+1)
-  # Create or load pickle
+  def __init__(self, input: str, budget: int, processor: TrOCRProcessor, model: VisionEncoderDecoderModel, device: str, toxic_model: ModelWrapper, gold_label_toxic: bool):
+      super().__init__(input, budget, processor, model, device)
+      self.toxic_model = toxic_model
+      self.gold_label_toxic = gold_label_toxic
+
+  def objective(self) -> Callable[[List[float]], float]:
+    ''' Tries to make all inputs classify as non-toxic. '''
+    def _objective(perturbations: List[float]) -> float:
+      candidate = self.candidate(perturbations)
+      if candidate in self.cache:
+        output = self.cache[candidate]
+      else:
+        output = self.toxic_model.predict([self.ocr(candidate)])[0]['toxic']
+        self.cache[candidate] = output
+      return output
+    return _objective
+
+  def differential_evolution(self, maxiter: int, popsize: int) -> Dict:
+    adv_example = super().differential_evolution(maxiter, popsize)
+    adv_logit_toxic = self.toxic_model.predict([adv_example['adv_example']])[0]['toxic']
+    adv_label_toxic = round(adv_logit_toxic) == 1
+    return serialize_toxic(adv_label_toxic, self.gold_label_toxic, adv_logit_toxic, **adv_example)
+
+
+def create_or_load_pickle(pkl_file: str, label: str, overwrite: bool) -> Dict:
   if overwrite or not exists(pkl_file):
     adv_examples = { label: {} }
   else:
@@ -140,6 +181,25 @@ def trocr_experiment(start_index: int, end_index: int, min_budget: int, max_budg
       adv_examples = pickle.load(f)
     if label not in adv_examples:
       adv_examples[label] = {}
+  return adv_examples
+
+def load_trocr(cpu: bool):
+  # The "printed" models output all uppercase, so we use the "handwritten" models
+  # which appear to perform well on computer-generated text as well
+  device = 'cuda:0' if not cpu and cuda.is_available() else 'cpu'
+  processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
+  model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten').to(device)
+  print(f"TrOCR configured to use device {device}.")
+  return device, processor, model
+
+def trocr_experiment(start_index: int, end_index: int, min_budget: int, max_budget: int, pkl_file: str, maxiter: int, popsize: int, overwrite: bool, cpu: bool, **kwargs):
+  # Load resources
+  label = "trocr-diacriticals"
+  dataset = load_glue_data(start_index, end_index)
+  print(f"Performing experiments against the TrOCR model for {len(dataset)} examples, from index {start_index} to {end_index}, with budgets {min_budget} to {max_budget}.")
+  device, processor, model = load_trocr(cpu)
+  budgets = range(min_budget, max_budget+1)
+  adv_examples = create_or_load_pickle(pkl_file, label, overwrite)
   # Run experiments
   with tqdm(total=len(dataset)*len(budgets), desc="Adv. Examples") as pbar:
     for budget in budgets:
@@ -153,12 +213,42 @@ def trocr_experiment(start_index: int, end_index: int, min_budget: int, max_budg
             pickle.dump(adv_examples, f)
         pbar.update(1)
 
+def toxic_experiment(start_index: int, end_index: int, min_budget: int, max_budget: int, pkl_file: str, maxiter: int, popsize: int, overwrite: bool, cpu: bool, **kwargs):
+  # Load resources
+  label = "toxic-diacriticals"
+  dataset = load_toxic_data(start_index, end_index)
+  print(f"Performing experiments against the IBM MaxToxic model for {len(dataset)} examples, from index {start_index} to {end_index}, with budgets {min_budget} to {max_budget}.")
+  device, processor, model = load_trocr(cpu)
+  getLogger().setLevel(WARNING)
+  toxic_model = ModelWrapper()
+  if not cpu and cuda.is_available():
+    toxic_model.model.cuda()
+    toxic_model.device = torchdevice("cuda")
+    device = "cuda"
+  else:
+    device = "cpu"
+  print(f"MaxToxic model configured to use device {device}.")
+  budgets = range(min_budget, max_budget+1)
+  adv_examples = create_or_load_pickle(pkl_file, label, overwrite)
+  # Run experiments
+  with tqdm(total=len(dataset)*len(budgets), desc="Adv. Examples") as pbar:
+    for budget in budgets:
+      if budget not in adv_examples[label]:
+        adv_examples[label][budget] = {}
+      for data in dataset:
+        if str(data['rev_id']) not in adv_examples[label][budget]:
+          objective = TrOcrObjective(data['comment'], budget, processor, model, device)
+          adv_examples[label][budget][str(data['rev_id'])] = objective.differential_evolution(maxiter, popsize)
+          with open(pkl_file, 'wb') as f:
+            pickle.dump(adv_examples, f)
+        pbar.update(1)
 
 if __name__ == '__main__':
 
   parser = ArgumentParser(description='Adversarial NLP Experiments.')
   model = parser.add_mutually_exclusive_group(required=True)
   model.add_argument('-t', '--trocr', action='store_true', help="Target Microsoft TrOCR model.")
+  model.add_argument('-x', '--toxic', action='store_true', help="Target IBM's MaxToxic model defended by TrOCR.")
   parser.add_argument('-c', '--cpu', action='store_true', help="Use CPU for ML inference instead of CUDA.")
   parser.add_argument('pkl_file', help="File to contain Python pickled output.")
   parser.add_argument('-s', '--start-index', type=int, default=0, help="The lower bound of the items in the dataset to use in experiments.")
@@ -176,3 +266,6 @@ if __name__ == '__main__':
 
   if args.trocr:
     trocr_experiment(**vars(args))
+
+  elif args.toxic:
+    toxic_experiment(**vars(args))
--- experiments/setup.sh
+++ experiments/setup.sh
@@ -1,2 +1,17 @@
-pip install transformers datasets textdistance[extras] scipy tqdm torch Pillow
+pip install transformers datasets textdistance[extras] scipy tqdm torch Pillow pandas
 curl -s https://raw.githubusercontent.com/kaienfr/Font/master/font/ARIALUNI.TTF --output arialuni.ttf
+rm -rf assets/
+rm -rf toxic/
+mkdir assets
+wget https://max-cdn.cdn.appdomain.cloud/max-toxic-comment-classifier/1.0.0/assets.tar.gz --output-document=assets/assets.tar.gz --no-check-certificate
+tar -x -C assets/ -f assets/assets.tar.gz -v
+rm assets/assets.tar.gz
+git clone https://github.com/IBM/MAX-Toxic-Comment-Classifier.git
+mv MAX-Toxic-Comment-Classifier toxic
+sed -i 's/==.*//g' toxic/requirements.txt
+pip install -r toxic/requirements.txt
+pip install maxfw
+sed -i 's/from config/from ..config/g' toxic/core/model.py
+sed -i 's/from core\./from ./g' toxic/core/model.py
+wget https://ndownloader.figshare.com/files/7394542 -O toxicity_annotated_comments.tsv
+wget https://ndownloader.figshare.com/files/7394539 -O toxicity_annotations.tsv
